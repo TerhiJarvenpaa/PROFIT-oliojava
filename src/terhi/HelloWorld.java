82857e4db3c7746f2762179f950e296d1bffaa02
TerhiJarvenpaa <terhi.jarvenpaa@tieto.com>
2015-01-27 10:21:46 +0200

    Muutettu HelloWorld ja lisätty Laskin

--- src/terhi/HelloWorld.java
+++ src/terhi/HelloWorld.java
@@ -13,11 +13,25 @@ public class HelloWorld {
  * 
  */
    
-    
+    private static String pieniNimi;
+    private static String isoNimi;
     
     public void yellToWorld(String nimi){
+        
+        
         System.out.println("Hello world it's "+ nimi + " here");
 
+    }
+    
+    /**
+     * @param nimi annettava nimi
+     * @return nimi isolla kirjoitettuna
+     */
+     private static String muutaIsoksi(String pieniNimi){
+    
+        isoNimi = pieniNimi.toUpperCase();
+        //System.out.println(isoNimi);
+        return isoNimi;
     }
 
     public static void main(String[] args) {
@@ -25,8 +39,16 @@ public class HelloWorld {
          * main
          */
         HelloWorld terhi = new HelloWorld();
-        terhi.yellToWorld("Terhi");
+        
+       terhi.yellToWorld("Terhi");
+       
+     //Muutetaan nimi isoiksi kirjaimiksi
+       HelloWorld terttu = new HelloWorld();
+       terttu.muutaIsoksi("Terttu");
+       terttu.yellToWorld(isoNimi);
         
     }
 
+
 }
+
